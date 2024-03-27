a731753f39b6b3a3faee1fc40423077ec245d71d
Ayse Nur Gezgin <aysenurgezgin99@gmail.com>
2024-03-27 07:38:17 +0300

    started the business

--- FriendlyCode/FriendlyCode.Mvc/Controllers/HomeController.cs
+++ FriendlyCode/FriendlyCode.Mvc/Controllers/HomeController.cs
@@ -17,3 +17,5 @@ namespace FriendlyCode.Mvc.Controllers
     }
 }
 //Ana sayfanın açılması demek Mvc uygulamasında;Home Controller'ın Index action'ı tetikleniyordu.(Eğer Controller ve Action söylenmezse default olarakController home,action olarak ta index olsun demiştik Program.cste)pattern=isteğin yapısı,deseni nasıl olacak.
+//Öncelikle sanki business katmanında yazılmış bir methot varmışta onu çağırıyoruz.O yüzden Business a gidip yaptk.
+//Soyutlamanın anlamı =Aynı yapıda ki bir sürü şeyi kullanma ihtiyacı duyabilirim o yüzden omum soyut bir halini oluşturayım ne zaman istersem onun somutlarını oluştururum mantığıdır.Şu anda da Trainer'larla ilgileniyorum,Tarinerları temsil eden bir interface oluşturayım ve bu interface'in içine(methotlar) bütün Trainerları getir,id'si şu olan Trainer'ı getir,yeni bir Trainer oluştur,Trainer'ı edit/sil gibi temel işlemlerini(CRUD) temsil eden methotları içinde barındıracak bir Trainer classı oluşturmak üzere Business a geçtim.Bunların somutlarını da oluşturmalıyım somut nesne lazım olduğu için içine kod yazacağım;somut ve soyutlar için iki ayrı klasör oluşturdum.(soyut yapı=interface ve abstarct classlar,somut(concrete)=classlar )
